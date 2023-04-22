80c43534907a21ebde908c8ba3a33a25094fa5fb
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-04-22 15:05:12 +0530

    file system add

---
+++ data.js
@@ -0,0 +1,7 @@
+const data =[
+    {name:'abhishek Bhoir',email:'abhi@bhoir.com'},
+    {name:'raj Bhoir',email:'raj@bhoir.com'},
+    {name:'parth Bhoir',email:'parth@bhoir.com'},
+]
+
+module.exports= data
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const http = require('http');
+// to chek theck the status and data
+
+// const http = require('http')
+// const data = require('./data')
 
 // http.createServer((req,resp)=>{
-//     resp.write("Hello this is my first node js program")
-//     resp.end();
-// }).listen(3500)
+// resp.writeHead(200,{'Content-Type':'application-json'});
+// resp.write(JSON.stringify(data));
+// resp.end();
+// }).listen(3200)
 
-function dataControl(req,resp) {
-    resp.write("Hello, wolrd in node js");
-    resp.end();
-    
+const fs = require("fs");
+const input = process.argv;
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+}else if (input[2]=='remove'){
+    fs.unlinkSync(input[3])
+}else{
+    console.log("invalid input");
 }
-
-http.createServer(dataControl).listen(3501);
