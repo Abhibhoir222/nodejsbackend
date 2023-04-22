4f1ade941066ec3cedb4cc8c757c24dc1354a014
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-04-22 15:50:07 +0530

    final commit before file added

--- index.js
+++ index.js
@@ -9,13 +9,26 @@
 // resp.end();
 // }).listen(3200)
 
-const fs = require("fs");
-const input = process.argv;
+// const fs = require("fs");
+// const input = process.argv;
 
-if (input[2] == "add") {
-  fs.writeFileSync(input[3], input[4]);
-}else if (input[2]=='remove'){
-    fs.unlinkSync(input[3])
-}else{
-    console.log("invalid input");
-}
+// if (input[2] == "add") {
+//   fs.writeFileSync(input[3], input[4]);
+// }else if (input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+// }else{
+//     console.log("invalid input");
+// }
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname,'files');
+
+// for(i =0;i <5;i++){
+//     fs.writeFileSync(dirPath+"/hello"+i+".txt","a simple file");
+// }
+
+fs.readdir(dirPath,(err,files)=>{
+    files.forEach((item)=>{
+        console.log(item);
+    })
+})
