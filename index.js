121974d1c8a0a9449c5d5fc8d5d705015b1d7118
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-04-24 10:54:59 +0530

    extation hidden

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname,'public')
 
+app.get('',(req,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
+app.get('/help',(req,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+})
+app.get('*',(req,resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`)
+})
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 app.listen(3200);   
