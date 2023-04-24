b34b25ff27ee48ec9e64b99bd87c2ec00ebab765
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-04-24 10:34:30 +0530

    load static html

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname,'public')
 
-app.get("",(req,resp)=>{
-    // resp.send("Welcome, to the Home Page")
-    resp.send(`
-    <h1>hellow  this is welcome page </h1> <a href="/about"> Go to about page </a>
-    `);
-})
 
-app.get("/about",(req,resp)=>{
-    // resp.send("Welcome, to the about Page")
-    resp.send(`
-    <input type="text" placeholder="User name"/>
-    <button> Click Me </button>
-    <a href="/"> Go to home page</a>
-    `);
-})
-
-app.get("/about",(req,resp)=>{
-    // resp.send("Welcome, to the about Page")
-    resp.send(`
-    <input type="text" placeholder="User name" value="${req.query.name}"/>
-    <button> Click Me </button>
-    <a href="/"> Go to home page</a>
-    `);
-})
-
+app.use(express.static(publicPath));
 app.listen(3200);   
